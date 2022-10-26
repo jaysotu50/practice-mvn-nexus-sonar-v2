d7d9fd9f0f143504ea549fb27235d70739d67c22
jaysotu50 <99056298+jaysotu50@users.noreply.github.com>
2022-10-25 20:26:15 -0500

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,14 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		String name ="vammsi"
-
+        String name = "vamsi";
+        
 		System.out.println(name);
-
-		Integer age = "20";
-		System.out.println(age + 5);
-	}
-
+    }
 }
+
